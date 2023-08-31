18532595ee8793201abc1137d1fe329d6e25a027
januar7 <jnrgamingmania0343@gmail.com>
2023-08-31 03:42:08 +0000

    Now i tried to transversed what i make on local repo to gitHub

---
+++ example1.py
@@ -0,0 +1,8 @@
+# This one just a simple program using  if condition
+def even_odd(number):
+    if int(number)%2==0:
+        print("{} is an even number".format(number))
+    else:
+        print("{} is an odd number".format(number)
+
+even_odd(number=5)
